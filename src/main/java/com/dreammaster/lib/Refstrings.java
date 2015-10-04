369e067f3554bf5eb3c2762e4a78611d479b6935
Namikon <namikon@gmx.net>
2015-10-04 21:11:35 +0200

    Whoops..

--- src/main/java/com/dreammaster/lib/Refstrings.java
+++ src/main/java/com/dreammaster/lib/Refstrings.java
@@ -6,7 +6,7 @@ public class Refstrings {
 	public static final String NAME = "GT: New Horizons";
 	public static final String VERSION = "GRADLETOKEN_VERSION";
 	public static final String CLIENTSIDE = "com.dreammaster.main.ClientProxy";
-	public static final String SERVERSIDE = "com.dreammaster.main.ServerProxy";
+	public static final String SERVERSIDE = "com.dreammaster.main.CommonProxy";
 	public static final String COLLECTIONID = "GTNewHorizons";
 
 }
