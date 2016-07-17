18098fb46adab0181e464632f399e4e1882e5660
Dream-Master <dream-master@gmx.net>
2016-07-17 17:08:20 +0200

    Added Alchemical Wand Cap for Forbidden MAgic

--- src/main/java/com/dreammaster/item/ItemList.java
+++ src/main/java/com/dreammaster/item/ItemList.java
@@ -316,6 +316,7 @@ public enum ItemList
     VoidPlate(new ModSimpleBaseItem("VoidPlate", ModTabList.ModThaumcraftTab)),
     VoidRing(new ModSimpleBaseItem("VoidRing", ModTabList.ModThaumcraftTab)),
     VoidRod(new ModSimpleBaseItem("VoidRod", ModTabList.ModThaumcraftTab)),
+    WandCapAlchemical(new ModSimpleBaseItem("WandCapAlchemical", ModTabList.ModThaumcraftTab)),
     WandCapBloodIron(new ModSimpleBaseItem("WandCapBloodIron", ModTabList.ModThaumcraftTab)),
     WeakOrb(new ModSimpleBaseItem("WeakOrb", ModTabList.ModThaumcraftTab)),
     WetTofu(new ModSimpleBaseItem("WetTofu", ModTabList.ModGenericTab)),
