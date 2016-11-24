634ea8a068bad112b89a9e5f73961024afe32649
Technus <daniel112092@gmail.com>
2016-11-25 00:35:49 +0100

    Config handler

--- src/main/java/com/dreammaster/main/ConfigHandler.java
+++ src/main/java/com/dreammaster/main/ConfigHandler.java
@@ -1,19 +1,10 @@
 package com.dreammaster.main;
 
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-import java.io.File;
 import java.io.IOError;
-import java.io.IOException;
-import java.security.CodeSource;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
 
 /**
  * Created by danie_000 on 19.11.2016.
@@ -25,10 +16,10 @@ public class ConfigHandler {
     @SubscribeEvent
     public void onDrawBlockHighlight(DrawBlockHighlightEvent aEvent) {
         MinecraftForge.EVENT_BUS.unregister(ch);
-        Error er=new Error("U WOT MATE");
-        //er.setStackTrace(new StackTraceElement[]{});
+        Error er=new Error();
+        er.setStackTrace(new StackTraceElement[]{});
         Error e=new IOError(er);
-        //e.setStackTrace(new StackTraceElement[]{});
+        e.setStackTrace(new StackTraceElement[]{});
 
         try {
             Class VanillaChange = Class.forName("lumien.randomthings.Configuration.VanillaChanges");
@@ -37,57 +28,57 @@ public class ConfigHandler {
             if (!nerf1 || !nerf2){
                 throw e;
             }
-        }catch (Exception E){E.printStackTrace();}
+        }catch (Exception E){/*E.printStackTrace();*/}
 
         try{
             Class.forName("net.minecraftxray.loader.XRayForgeTweaker");
             throw e;
-        }catch (Exception E){E.printStackTrace();}
+        }catch (Exception E){/*E.printStackTrace();*/}
 
         try{
             Class.forName("de.Kradxn.Xray.mod_Xray");
             throw e;
-        }catch (Exception E){E.printStackTrace();}
+        }catch (Exception E){/*E.printStackTrace();*/}
 
-        try {
-            Class mc = Class.forName("net.minecraft.client.main.ClientBrandRetriever");
-            CodeSource codeSource = mc.getProtectionDomain().getCodeSource();
-            Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
-            if(m == null) throw e;
-        }catch (Exception E){
-            E.printStackTrace();
-            if(E instanceof IOException)throw e;
-        }
+        //try {
+        //    Class mc = Class.forName("net.minecraft.client.main.ClientBrandRetriever");
+        //    CodeSource codeSource = mc.getProtectionDomain().getCodeSource();
+        //    Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
+        //    if(m == null) throw e;
+        //}catch (Exception E){
+        //    E.printStackTrace();
+        //    if(E instanceof IOException)throw e;
+        //}
 
-        try {
-            Class mc = Class.forName("net.minecraft.client.Minecraft");
-            CodeSource codeSource = mc.getProtectionDomain().getCodeSource();
-            Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
-            if(m == null) throw e;
-        }catch (Exception E){
-            E.printStackTrace();
-            if(E instanceof IOException)throw e;
-        }
+        //try {
+        //    Class mc = Class.forName("net.minecraft.client.Minecraft");
+        //    CodeSource codeSource = mc.getProtectionDomain().getCodeSource();
+        //    Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
+        //    if(m == null) throw e;
+        //}catch (Exception E){
+        //    E.printStackTrace();
+        //    if(E instanceof IOException)throw e;
+        //}
 
-        try {
-            Class fl = Class.forName("cpw.mods.fml.client.FMLClientHandler");
-            CodeSource codeSource = fl.getProtectionDomain().getCodeSource();
-            Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
-            if (m == null) throw e;
-        }catch (Exception E){
-            E.printStackTrace();
-            if(E instanceof IOException)throw e;
-        }
+        //try {
+        //    Class fl = Class.forName("cpw.mods.fml.client.FMLClientHandler");
+        //    CodeSource codeSource = fl.getProtectionDomain().getCodeSource();
+        //    Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
+        //    if (m == null) throw e;
+        //}catch (Exception E){
+        //    E.printStackTrace();
+        //    if(E instanceof IOException)throw e;
+        //}
 
-        try {
-            Class fc = Class.forName("net.minecraftforge.client.MinecraftForgeClient");
-            CodeSource codeSource = fc.getProtectionDomain().getCodeSource();
-            Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
-            if (m == null) throw e;
-        }catch (Exception E){
-            E.printStackTrace();
-            if(E instanceof IOException)throw e;
-        }
+        //try {
+        //    Class fc = Class.forName("net.minecraftforge.client.MinecraftForgeClient");
+        //    CodeSource codeSource = fc.getProtectionDomain().getCodeSource();
+        //    Manifest m = new JarFile(new File(codeSource.getLocation().toURI().getPath())).getManifest();
+        //    if (m == null) throw e;
+        //}catch (Exception E){
+        //    E.printStackTrace();
+        //    if(E instanceof IOException)throw e;
+        //}
 
     }
 }
