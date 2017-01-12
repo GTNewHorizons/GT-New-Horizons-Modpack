dc12e3e1e44d20b1485eab230760baa372233f56
Dream-Master <dream-master@gmx.net>
2017-01-12 21:31:03 +0100

    remove import

--- src/main/java/com/dreammaster/modfixes/avaritia/SkullFireSwordDropFix.java
+++ src/main/java/com/dreammaster/modfixes/avaritia/SkullFireSwordDropFix.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
