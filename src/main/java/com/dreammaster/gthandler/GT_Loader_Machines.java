0be381ccb077bbaa7c766aed2e7fac66d86ceb63
Dream-Master <dream-master@gmx.net>
2017-01-18 12:04:15 +0100

    Add Chest Buffer higher Tiers
    Not sure if gives it ID 9400 @Technus

--- src/main/java/com/dreammaster/gthandler/GT_Loader_Machines.java
+++ src/main/java/com/dreammaster/gthandler/GT_Loader_Machines.java
@@ -12,6 +12,7 @@ import gregtech.api.interfaces.ITexture;
 import gregtech.api.metatileentity.implementations.*;
 import gregtech.api.util.GT_ModHandler;
 import gregtech.api.util.GT_Recipe;
+import gregtech.common.tileentities.automation.GT_MetaTileEntity_Regulator;
 import gregtech.common.tileentities.generators.GT_MetaTileEntity_PlasmaGenerator;
 import gregtech.common.tileentities.machines.basic.*;
 import net.minecraft.item.ItemStack;
@@ -31,6 +32,60 @@ public class GT_Loader_Machines
 
 		// MetaTileEntity ID Range: look at end of file
 		// GT 5.08.30
+
+
+		//TODO add Conveyor Modules for more Tiers
+		// ===================================================================================================
+		// Automated Chest Buffer
+		// ===================================================================================================
+		CustomItemList.Automation_ChestBuffer_UHV.set(new GT_MetaTileEntity_Regulator(
+				9400, "automation.chestbuffer.tier.10", "Ultra Low Voltage Regulator", 10).getStackForm(1L));
+
+		CustomItemList.Automation_ChestBuffer_UEV.set(new GT_MetaTileEntity_Regulator(
+				9401, "automation.chestbuffer.tier.11", "Low Voltage Regulator", 11).getStackForm(1L));
+
+		CustomItemList.Automation_ChestBuffer_UIV.set(new GT_MetaTileEntity_Regulator(
+				9402, "automation.chestbuffer.tier.12", "Medium Voltage Regulator", 12).getStackForm(1L));
+
+		CustomItemList.Automation_ChestBuffer_UMV.set(new GT_MetaTileEntity_Regulator(
+				9403, "automation.chestbuffer.tier.13", "High Voltage Regulator", 13).getStackForm(1L));
+
+		GT_ModHandler.addCraftingRecipe(CustomItemList.Automation_ChestBuffer_UHV.get(1L, new Object[0]),
+				GT_ModHandler.RecipeBits.DISMANTLEABLE | GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.BUFFERED,
+				new Object[]{"CMV", " X ",
+						'M', ItemList.Hull_MAX,
+						'V', ItemList.Conveyor_Module_UV,
+						'C', OreDictNames.craftingChest,
+						'X', OrePrefixes.circuit.get(Materials.Basic)});
+
+		GT_ModHandler.addCraftingRecipe(CustomItemList.Automation_ChestBuffer_UHV.get(1L, new Object[0]),
+				GT_ModHandler.RecipeBits.DISMANTLEABLE | GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.BUFFERED,
+				new Object[]{"CMV", " X ",
+						'M', CustomItemList.Hull_UEV,
+						'V', ItemList.Conveyor_Module_UV,
+						'C', OreDictNames.craftingChest,
+						'X', OrePrefixes.circuit.get(Materials.Basic)});
+
+		GT_ModHandler.addCraftingRecipe(CustomItemList.Automation_ChestBuffer_UHV.get(1L, new Object[0]),
+				GT_ModHandler.RecipeBits.DISMANTLEABLE | GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.BUFFERED,
+				new Object[]{"CMV", " X ",
+						'M', CustomItemList.Hull_UIV,
+						'V', ItemList.Conveyor_Module_UV,
+						'C', OreDictNames.craftingChest,
+						'X', OrePrefixes.circuit.get(Materials.Basic)});
+
+		GT_ModHandler.addCraftingRecipe(CustomItemList.Automation_ChestBuffer_UHV.get(1L, new Object[0]),
+				GT_ModHandler.RecipeBits.DISMANTLEABLE | GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.BUFFERED,
+				new Object[]{"CMV", " X ",
+						'M', CustomItemList.Hull_UMV,
+						'V', ItemList.Conveyor_Module_UV,
+						'C', OreDictNames.craftingChest,
+						'X', OrePrefixes.circuit.get(Materials.Basic)});
+
+
+		// ===================================================================================================
+		// Plasma Generators
+		// ===================================================================================================
 
 		CustomItemList.Generator_Plasma_ZPMV.set(new GT_MetaTileEntity_PlasmaGenerator(
 				10752, "basicgenerator.plasmagenerator.tier.08", "Plasma Generator Mark IV",7).getStackForm(1L));
