47d722deecfb947182a01ad21c2de5fa2cf64590
Dream-Master <dream-master@gmx.net>
2017-07-02 16:14:26 +0200

    fix derp

--- src/main/java/com/dreammaster/block/QuantinumProperties.java
+++ src/main/java/com/dreammaster/block/QuantinumProperties.java
@@ -35,7 +35,7 @@ public class QuantinumProperties implements IExtendedBlockProperties
     public float getResistance() { return 30.0F; }
 
     @Override
-    public String getBlockName() { return "Quantium"; }
+    public String getBlockName() { return "Quantinum"; }
 
     @Override
     public String getTextureName() { return ""; }
