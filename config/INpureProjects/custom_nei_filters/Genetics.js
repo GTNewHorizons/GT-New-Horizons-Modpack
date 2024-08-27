c4c01956869f0bff97fd54ceb8213f8728d1f8b1
Martin Robertz <dream-master@gmx.net>
2024-08-27 10:04:07 +0200

    remove inpure mod

--- config/INpureProjects/custom_nei_filters/Genetics.js
+++
@@ -1,4 +0,0 @@
-if (FML.isModLoaded("Genetics") && Genetics_enabled) {
-    NEI.override_with_nbt("Genetics", "serum");
-    NEI.override_with_nbt("Genetics", "serumArray");
-}
--- config/INpureProjects/custom_nei_filters/TecTech.js
+++
@@ -1,3 +0,0 @@
-if (FML.isModLoaded("tectech") && tectech_enabled) {
-    NEI.hide("tectech:item.em.debugContainer");
-}
